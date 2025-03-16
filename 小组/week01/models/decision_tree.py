4501b301b3eb33c739dd785777ae5a80049de215
TGMax233_Michael <tgmax233michael@163.com>
2025-03-17 00:41:27 +0800

    update

--- 小组/week01/models/decision_tree.py
+++ 小组/week01/models/decision_tree.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+# 计算基尼指数
 def gini(y: np.ndarray):
     labels, counts = np.unique(y, return_counts=True)
     ratio = counts / len(y)
     return 1 - np.sum(ratio**2)
 
+# 计算均方误差
+def MSE(y: np.ndarray):
+    return 1/len(y) * np.sum((y - np.mean(y))**2)
+
+# 树节点
 class DecisionTreeNode:
     def __init__(self, feature=None, threshold=None, left=None, right=None, label=None):
         self.feature = feature
@@ -14,10 +20,19 @@ class DecisionTreeNode:
         self.label = label
 
 class TreeClassifier:
-    def __init__(self):
+    """
+        分类决策树
+        Attributes:
+            max_depth: 最深层数
+    """
+    def __init__(self, max_depth=100):
         self.tree: DecisionTreeNode|None = None
+        self.max_depth = max_depth
     
     def _best_split(self, X: np.ndarray, y: np.ndarray):
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+        
         n_samples, n_features = X.shape
         best_threshold = None
         best_feature = None
@@ -25,7 +40,7 @@ class TreeClassifier:
         best_gain = -np.inf
         
         for feature in range(n_features):
-            sorted_uniques = np.unique(X[:, feature])
+            sorted_uniques = np.sort(np.unique(X[:, feature]))
             if len(sorted_uniques) == 1:
                 thresholds = sorted_uniques
             else:
@@ -48,20 +63,23 @@ class TreeClassifier:
                     
         return best_feature, best_threshold
     
-    def _build_tree(self, X: np.ndarray, y: np.ndarray):
+    def _build_tree(self, X: np.ndarray, y: np.ndarray, layer):
         if len(np.unique(y)) == 1:
             return DecisionTreeNode(label=y[0])
+        if layer == self.max_depth:
+            # 将出现最多的类别数作为标签
+            return DecisionTreeNode(label=np.argmax(np.bincount(y)))
         
         feature, threshold = self._best_split(X, y)
         
         left_mask = X[:, feature] > threshold
         right_mask = ~left_mask
-        left = self._build_tree(X[left_mask], y[left_mask])
-        right = self._build_tree(X[right_mask], y[right_mask])
+        left = self._build_tree(X[left_mask], y[left_mask], layer+1)
+        right = self._build_tree(X[right_mask], y[right_mask], layer+1)
         
         return DecisionTreeNode(feature, threshold, left, right)
     
-    def _travel_tree(self, X: np.ndarray, node: DecisionTreeNode):
+    def _travel_tree(self, X: np.ndarray, node: DecisionTreeNode, layer):
         if node is None:
             return None
         if node.label is not None:
@@ -71,8 +89,8 @@ class TreeClassifier:
         left_mask = X[:, node.feature] > node.threshold
         right_mask = ~left_mask
         
-        predictions[left_mask] = self._travel_tree(X[left_mask], node.left)
-        predictions[right_mask] = self._travel_tree(X[right_mask], node.right)
+        predictions[left_mask] = self._travel_tree(X[left_mask], node.left, layer+1)
+        predictions[right_mask] = self._travel_tree(X[right_mask], node.right, layer+1)
         
         return predictions
         
@@ -80,8 +98,59 @@ class TreeClassifier:
         if X.ndim == 1:
             X = X.reshape(-1, 1)
             
-        self.tree = self._build_tree(X, y)
+        self.tree = self._build_tree(X, y, 1)
         
     def predict(self, X: np.ndarray):
         node = self.tree
-        return self._travel_tree(X, node)
+        return self._travel_tree(X, node, 0)
+    
+class TreeRegressor:
+    def __init__(self):
+        self.tree: DecisionTreeNode|None = None
+    
+    def _best_split(X: np.ndarray, y: np.ndarray):
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+        
+        n_samples, n_featurs = X.shape
+        best_feature = None
+        best_threshold = None
+        best_gain = -np.inf
+        base_MSE = MSE(y)
+        
+        for feature in range(n_featurs):
+            sorted_unique = np.sort(np.unique(y))
+            if len(sorted_unique) == 1:
+                thresholds = sorted_unique
+            else:
+                thresholds = (sorted_unique[:-1] + sorted_unique[1:]) / 2
+            
+            for threshold in thresholds:
+                left_mask = X[:, feature] < left_mask
+                right_mask = ~left_mask
+                
+                weighted_MSE = len(y[left_mask])/len(y) * MSE(y[left_mask]) + len(y[right_mask])/len(y) * MSE(y[right_mask])
+                gain = base_MSE - weighted_MSE
+                
+                if gain > best_gain:
+                    best_gain = gain
+                    best_feature = feature
+                    best_threshold = threshold
+                    
+        return best_feature, best_threshold
+    
+    # 等待完成
+    def _build_tree(self, X: np.ndarray, y: np.ndarray):
+        ...
+        
+    def _travel_tree(self, X: np.ndarray, node):
+        ...
+        
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        ...
+        
+    def predict(self, X: np.ndarray):
+        ...
+                
+        
+         
--- 小组/week01/决策树测试/main.py
+++ 小组/week01/决策树测试/main.py
@@ -5,15 +5,15 @@ from model_selection import train_test_split
 
 if __name__ == "__main__":
     # data = load_iris()
-    data = make_classification(n_samples=1000, n_classes=2, 
-                               n_features=4, n_informative=4, 
-                               n_redundant=0, n_repeated=0, 
+    data = make_classification(n_samples=1000, n_classes=3, 
+                               n_features=5, n_informative=4, 
+                               n_redundant=1, n_repeated=0, 
                                random_state=42)
     
     # X, y = data["data"], data["target"]
     X, y = data[0], data[-1]
     X_train, X_test, y_train, y_test = train_test_split(X, y, 42, 0.2)
-    model = TreeClassifier()
+    model = TreeClassifier(max_depth=20)
     
     model.fit(X_train, y_train)
     y_train_pred = model.predict(X_train)
@@ -23,6 +23,3 @@ if __name__ == "__main__":
     print(f"Accuracy: {Accuracy(y_train, y_train_pred)}")
     print("测试集")
     print(f"Accuracy: {Accuracy(y_test, y_test_pred)}")
-    
-    
-    
