f16b34d1ca49c845290306fdc4bf7435183d245d
TGMax233_Michael <tgmax233michael@163.com>
2025-03-27 16:07:45 +0800

    update

--- 小组/week01/决策树测试/test.py
+++ 小组/week01/决策树测试/test.py
@@ -26,37 +26,37 @@ def best_split(X, y):
     best_feature = None
     base_gini = gini(y)
     best_gain = -np.inf
-    
+
     for feature in range(n_featurs):
         # print(f"feature: {feature}")
-        
+
         sorted_uniques = np.unique(np.sort(X[:, feature]))
-        
+
         # 选取阈值
         thresholds = (sorted_uniques[:-1] + sorted_uniques[1:])/2 if len(sorted_uniques) > 1 else sorted_uniques
-        
+
         for threshold in thresholds:
             left_mask = X[:, feature] > threshold
             right_mask = X[:, feature] < threshold
-            
+
             y_left = y[left_mask]
             y_right = y[right_mask]
-            
+
             # print(f"y_left: {y_left}")
             # print(f"y_right: {y_right}")
-            
+
             # 计算加权基尼系数
             weighted_gini = (len(y_left) / n_samples * gini(y_left)) + (len(y_right) / n_samples * gini(y_right))
-            
+
             delta_gini = base_gini - weighted_gini
             # print(f"delta_gini: {delta_gini}")
             # print()
-            
+
             if delta_gini > best_gain:
                 best_gain = delta_gini
                 best_feature = feature
                 best_threshold = threshold
-                
+
     return best_feature, best_threshold
 
 class TreeNode:
@@ -66,20 +66,20 @@ class TreeNode:
         self.left = left
         self.right = right
         self.label = label
-    
+
 def build_tree(X: np.array, y: np.array):
     if len(np.unique(y)) == 1:
         return TreeNode(label=y[0])
-    
+
     feature, threshold = best_split(X, y)
-    
+
     left_mask = X[:, feature] < threshold
     right_mask = ~left_mask
-    
+
     left = build_tree(X[left_mask], y[left_mask])
     right = build_tree(X[right_mask], y[right_mask])
     return TreeNode(feature=feature, threshold=threshold, left=left, right=right)
-    
+
 tree = build_tree(X_train, y_train)
 
 def show_tree(node: TreeNode, depth=0):
@@ -89,30 +89,30 @@ def show_tree(node: TreeNode, depth=0):
     if node.label is not None:
         print(f"label: {node.label}")
         return
-    
+
     print(f"feature: {node.feature} threshold: {node.threshold}")
-    
+
     print(f"{indent}L ->")
     show_tree(node.left, depth+1)
-    
+
     print(f"{indent}R ->")
     show_tree(node.right, depth+1)
 
-        
+
 
 def predict(X, node: TreeNode):
     if not node:
         return None
     if node.label is not None:
         return np.full(X.shape[0], node.label)
-    
+
     left_mask = X[:, node.feature] < node.threshold
     right_mask = ~left_mask
-    
+
     prediction = np.zeros(X.shape[0], int)
     prediction[left_mask] = predict(X[left_mask], node.left)
     prediction[right_mask] = predict(X[right_mask], node.right)
-    
+
     return prediction
 
 show_tree(tree)
